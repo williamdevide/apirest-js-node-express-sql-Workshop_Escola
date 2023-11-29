739e379d0fde33463e71d92c3b3d60ea1785f1ac
williamdevide <williamdevide@gmail.com>
2023-11-29 14:16:20 -0300

    Alterações para poder consumir API no REACT

---
+++ insomnia/insomniaREADME.md
@@ -0,0 +1,1 @@
+Pasta para guardar o script exportado do INSOMNIA
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
@@ -13,6 +15,21 @@ import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
 
+const whiteList = [
+  'http://wdkdev.ddns.net',
+  'http://35.198.53.165',
+  'http://localhost:3002',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 class App {
   constructor() {
     this.app = express();
@@ -21,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
