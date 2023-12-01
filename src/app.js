f8279185e38d768e2f64dbfa8f4af7ed36fed985
williamdevide <williamdevide@gmail.com>
2023-12-01 14:51:19 -0300

    alteração no cors e helmet

--- src/app.js
+++ src/app.js
@@ -40,7 +40,10 @@ class App {
 
   middlewares() {
     this.app.use(cors(corsOptions));
-    this.app.use(helmet());
+    this.app.use(helmet({
+      crossOriginResourcePolicy: false,
+    }));
+
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
